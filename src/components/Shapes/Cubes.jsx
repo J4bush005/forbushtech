256ae58e79158ad8b09f2fa2ae85f78e88b00fa8
j4bush005 <jtforbush429@gmail.com>
2023-08-17 00:50:44 -0500

    working on phone screens

--- src/components/Shapes/Cubes.jsx
+++ src/components/Shapes/Cubes.jsx
@@ -1,6 +1,4 @@
-import {
-  OrbitControls
-} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { styled } from "styled-components";
@@ -12,19 +10,36 @@ const Container = styled.div`
   width: 100%;
   scroll-snap-align: center;
 `;
+const Containers = styled.div`
+  height: 90vh;
+  width: 100%;
+  padding-bottom: 50%;
+`;
 
 function Cube() {
   return (
-    <Media query="(min-width: 1024px)">
-    <Container>
-      <Canvas camera={{ fov: 35, position: [5, 5, 5] }}>
-        <OrbitControls enableZoom={false} autoRotate />
-        <ambientLight intensity={1} />
-        <directionalLight position={[3, 2, 1]} />
-        <Boost />
-      </Canvas>
-    </Container>
-    </Media>
+    <>
+      <Media query="(max-width: 768px)">
+        <Containers>
+          <Canvas camera={{ fov: 30, position: [5, 5, 5] }}>
+            <OrbitControls enableZoom={false} autoRotate />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Boost />
+          </Canvas>
+        </Containers>
+      </Media>
+      <Media query="(min-width: 1024px)">
+        <Container>
+          <Canvas camera={{ fov: 35, position: [5, 5, 5] }}>
+            <OrbitControls enableZoom={false} autoRotate />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Boost />
+          </Canvas>
+        </Container>
+      </Media>
+    </>
   );
 }
 
--- src/components/pages/Mission.jsx
+++ src/components/pages/Mission.jsx
@@ -4,37 +4,67 @@ import Media from "react-media";
 
 const Mission = () => {
   return (
-    <Media query="(min-width: 1024px)">
-      <div className="row">
-        <div className="col-6">
-          <Cube />
+    <>
+      <Media query="(max-width: 768px)">
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="col-12">
+            <div id="Heading-sm" className="display-4">
+              Our Commitment to Quality
+            </div>
+            <div className="flex">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
+            </div>
+            <div id="body-text-sm" className="r-text">
+              At Forbush Technologies, we stand as the embodiment of excellence
+              in all that we do. We are dedicated to pushing the boundaries of
+              innovation, producing solutions that set new standards in quality.
+              Our unwavering commitment to delivering excellence permeates every
+              facet of our work, from groundbreaking technological advancements
+              to unparalleled customer service. With a relentless pursuit of
+              perfection, we redefine what is possible, ensuring that each
+              endeavor we undertake is a testament to our uncompromising
+              dedication to excellence.
+            </div>
+          </div>
+          <div className="col">
+            <Cube />
+          </div>
         </div>
-        <h1 id="Heading" className="col-6 display-4">
-          Our Commitment to Quality
-          <div className="flex">
-            <div className="x1 goldbar home">
-              <div className="x1"></div>
-            </div>
+      </Media>
+      <Media query="(min-width: 1024px)">
+        <div className="row">
+          <div className="col-6">
+            <Cube />
           </div>
-          <div className="row">
-            <div className="col-12">
-              <p id="body-texts" className="fw-normal">
-                At Forbush Technologies, we stand as the embodiment of
-                excellence in all that we do. We are dedicated to pushing the
-                boundaries of innovation, producing solutions that set new
-                standards in quality. Our unwavering commitment to delivering
-                excellence permeates every facet of our work, from
-                groundbreaking technological advancements to unparalleled
-                customer service. With a relentless pursuit of perfection, we
-                redefine what is possible, ensuring that each endeavor we
-                undertake is a testament to our uncompromising dedication to
-                excellence.
-              </p>
+          <h1 id="Heading" className="col-6 display-4">
+            Our Commitment to Quality
+            <div className="flex">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
             </div>
-          </div>
-        </h1>
-      </div>
-    </Media>
+            <div className="row">
+              <div className="col-12">
+                <p id="body-texts" className="fw-normal">
+                  At Forbush Technologies, we stand as the embodiment of
+                  excellence in all that we do. We are dedicated to pushing the
+                  boundaries of innovation, producing solutions that set new
+                  standards in quality. Our unwavering commitment to delivering
+                  excellence permeates every facet of our work, from
+                  groundbreaking technological advancements to unparalleled
+                  customer service. With a relentless pursuit of perfection, we
+                  redefine what is possible, ensuring that each endeavor we
+                  undertake is a testament to our uncompromising dedication to
+                  excellence.
+                </p>
+              </div>
+            </div>
+          </h1>
+        </div>
+      </Media>
+    </>
   );
 };
 
